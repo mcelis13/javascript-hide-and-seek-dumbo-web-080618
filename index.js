0bdd0e0beb6487d1e121fe279124f18387ecbf8c
Madeline Celis <celiscb@vcu.edu>
2018-09-24 17:06:51 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+};
 
+function nestedTarget(){
+  return document.querySelector('#target');
+};
+
+function deepestChild(){
+  return document.querySelector('#grand-node div div div div');
+};
+
+function increaseRankBy(num){
+  let liNums = document.querySelectorAll('#app .ranked-list li');
+  for(let i = 0; i < liNums.length; i++){
+    liNums[i].innerHTML = parseInt(liNums[i].innerHTML) + num;
+  }
+}
